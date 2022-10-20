c9bcbf37f15b03bae85271fecd7b44405a9bbaeb
Nathan Cortinaz <nathancortinaz@hotmail.com>
2022-10-19 22:45:02 -0300

    Graphs functions created

--- src/Tests.py
+++ src/Tests.py
@@ -1,5 +1,53 @@
-def printa(texto):
-    print(texto)
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as pltline
+from openpyxl import load_workbook
+import openpyxl
+import os
 
+path = f'{os.path.dirname(os.path.realpath(__file__))}\Results'
 
-printa(texto="eh noix")
+reactions = [
+    ('neutral', 0),
+    ('happy', 1.744248399976641),
+    ('sad', 2.0440329000120983),
+    ('fear', 2.373049699992407),
+    ('sad', 2.75135879998561),
+    ('neutral', 3.3799759999965318),
+    ('sad', 3.6899851000052877),
+    ('neutral', 4.007389399979729),
+    ('sad', 4.617331699992064)]
+
+df = pd.DataFrame(reactions)
+
+# plt.figure(figsize=(9, 6))
+# plt.bar(x=df[0],
+#         height=df[1]
+#         )
+# plt.xticks(rotation=45)
+# plt.title("Gráfico", fontsize=25, fontweight='bold')
+# plt.xlabel('Reação')
+# plt.ylabel('Tempo')
+
+x = df[0]
+
+pltline.figure()
+pltline.stairs(x, fill=True)
+pltline.title("Gráfico", fontsize=25, fontweight='bold')
+pltline.ylabel('Reação')
+pltline.xlabel('Tempo')
+pltline.show()
+
+# pltline.plot(x, y, 'go--', linewidth=2, markersize=12)
+# pltline.title("Gráfico", fontsize=25, fontweight='bold')
+# pltline.ylabel('Reação')
+# pltline.xlabel('Tempo')
+# img = pltline.savefig(f"{path}/GraficoLinha.png")
+
+# wsnew = load_workbook(f"{path}/Results.xlsx")
+# newsheet = wsnew.create_sheet("Gráfico", 2)
+# img1 = openpyxl.drawing.image.Image(f"{path}/GraficoLinha.png")
+# img1.anchor = 'A1'
+# newsheet.add_image(img1)
+# wsnew.save(f"{path}/Results.xlsx")
+# wsnew.close()
--- src/app.py
+++ src/app.py
@@ -14,13 +14,16 @@ import cv2
 # biblioteca para acessar explorador de arquivo
 import subprocess
 
+# bibliotecas para gráficos
+import collections
+
 # definição do caminho frontend
 eel.init(f'{os.path.dirname(os.path.realpath(__file__))}/web')
 path = f'{os.path.dirname(os.path.realpath(__file__))}\Results'
 
 
 class FaceAnalyzer():
-    def __init__(self, capture, exit_character='q'):
+    def __init__(self, capture):
         self.capture = capture
         if not self.capture.isOpened():
             raise IOError("Não foi possível acessar webcam")
@@ -30,12 +33,14 @@ class FaceAnalyzer():
             self.new_reaction = 'neutral'
             self.face_found = False
             self.reactions = [(self.current_reaction, 0)]
+            self.reactions_step = []
+            self.sorted_frequency_list = []
             self.faceCascade = cv2.CascadeClassifier(
                 cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-            self.exit_character = exit_character
 
     def detect_reaction(self):
-        '''Check webcam video looking for face expressions'''
+        '''Verifica video da webcam buscando por expressões faciais'''
+
         try:
             success, self.frame = self.capture.read()
             predictions = DeepFace.analyze(self.frame, actions=['emotion'])
@@ -47,7 +52,8 @@ class FaceAnalyzer():
             self.face_found = False
 
     def show_faces(self):
-        '''Show faces detected in webcam with their expressions'''
+        '''Exibe faces detectadas na webcam com suas expressões'''
+
         if self.face_found == True:
             faces = self.faceCascade.detectMultiScale(self.frame, 1.1, 4)
             for (x, y, w, h) in faces:
@@ -59,28 +65,55 @@ class FaceAnalyzer():
             cv2.waitKey(1)
 
     def check_reaction(self):
-        '''Check if there is a new reaction and append it to reactions list'''
+        '''Verifica se há nova reação e adiciona à lista de reações'''
+
         if self.new_reaction != self.current_reaction:
             self.current_reaction = self.new_reaction
             running_time = perf_counter() - self.start
             self.reactions.append((self.new_reaction, running_time))
 
     def check_stop(self, debug_mode):
-        '''Check if user wants to stop and save reactions to file'''
-        # exitApp = cv2.waitKey(1) & 0xFF == ord(self.exit_character)
+        '''Verifica se usuário deseja parar e salva reações no arquivo'''
+
         if debug_mode == True:
             exitApp = eel.checkDebugButton()()
         else:
             exitApp = eel.checkRunButton()()
         if exitApp:
-            print(f'{self.reactions = }')
+            running_time = perf_counter() - self.start
+            self.reactions.append((self.current_reaction, running_time))
+            self.create_step_reactions_list()
+            self.create_frequency_list()
+            print(f'\n{self.reactions = }')
+            print(f'\n{self.reactions_step = }')
+            print(f'\n{self.sorted_frequency_list = }')
             results = pd.DataFrame(self.reactions)
-            results.to_excel(f"{path}/Results.xlsx", sheet_name='Detected Expresions')
+            results.to_excel(f"{path}/Results.xlsx", sheet_name='Expressões detectadas')
             return True
+
+    def create_step_reactions_list(self):
+        '''Cria lista com reações por segundo'''
+
+        current_reaction = 'neutral'
+        step = 0
+        for reaction in self.reactions:
+            while step < reaction[1]:
+                self.reactions_step.append((current_reaction, step))
+                step += 1
+            current_reaction = reaction[0]
+        self.reactions_step.append((reaction[0], step))
+
+    def create_frequency_list(self):
+        '''Cria lista com frequência de reações'''
+
+        reactions_list = [reaction[0] for reaction in self.reactions_step]
+        frequency_list = list(collections.Counter(reactions_list).items())
+        self.sorted_frequency_list = sorted(frequency_list, key=lambda x: (-x[1], x[0]))
 
 
 def start(debug_mode):
-    '''Starts debug mode'''
+    '''Inicia modo debug'''
+
     capture = cv2.VideoCapture(0)
     fa = FaceAnalyzer(capture)
     while True:
@@ -91,7 +124,7 @@ def start(debug_mode):
         stop_running = fa.check_stop(debug_mode)
         if stop_running == True:
             break
-        sleep(0.1)
+        sleep(1)
     capture.release()
     cv2.destroyAllWindows()
 
@@ -108,8 +141,9 @@ def debug():
 
 @eel.expose
 def open_results():
-    '''Open the folder containing the results files'''
+    '''Abre pasta contendo arquivo de resultados'''
+
     subprocess.Popen(f'explorer "{path}"')
 
 
-eel.start('index.html', size=(520, 235))
+eel.start('index.html', size=(620, 235))
